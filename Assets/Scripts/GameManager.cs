a7059293cb11cfe16c7ae6ae24cafcbc2d66f4c9
Wakz <rparedes@cocoschool.mx>
2024-04-21 22:09:59 -0600

    GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,33 +5,64 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    enum GameState {
+        None,
+        Play,
+        Pause,
+        GameOver
 
-    public float dayDuration; 
+    }
+
+    [SerializeField]GameState gameState;
+    public float dayDuration;
     private float timer;
-    
-    
+    [SerializeField]private int currentDay;
+
 
     void Update() {
 
-        timer -= Time.deltaTime;
+        if (gameState == GameState.Play) {
+            timer -= Time.deltaTime;
+        }
 
-   
         if (timer <= 0f) {
-                
-            Debug.Log("¡Tiempo fuera!");
-
-
+            if (currentDay == 3) {
+                changeGameState(GameState.GameOver);
+            }
             ChangeDay();
+            
         }
     }
 
     private void ChangeDay() {
-        
+        timer = dayDuration;
+        currentDay++;
     }
 
     void Start() {
         
         timer = dayDuration;
+        currentDay = 0;
+        changeGameState(GameState.Play);
     }
-    
+
+    void changeGameState(GameState newState) {
+        if (newState == gameState) {
+            return;
+        }
+
+        gameState = newState;
+
+        switch (gameState) {
+            case GameState.None: 
+                break;
+            case GameState.Play:
+                break;
+            case GameState.Pause:
+                break;
+            case GameState.GameOver:
+                break;
+
+        }
+    }
 }
