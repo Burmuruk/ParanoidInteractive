4867fcacd03fdf1acb40d14127d5344b7f5976c5
LAPURACHULETA <101416312+LAPURACHULETA@users.noreply.github.com>
2024-04-21 22:11:45 -0600

    estados

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,20 +4,75 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private int m_sanidadActual;
     //barra de sanidad
     //alucionaciones
     //alteracion de movimiento
+    BarStates barStates;
+    private void Start()
+    {
+        barStates = BarStates.Normal;
+    }
+    private void FixedUpdate()
+    {
+        DecisionManager();
+    }
+    void DecisionManager()
+    {
+        if (m_sanidadActual == 5)
+        {
+            barStates=BarStates.Normal;
+        }
+        else if (m_sanidadActual == 4)
+        {
+            barStates = BarStates.MasomenosNormal;
+        }
+        else if (m_sanidadActual == 3)
+        {
+            barStates = BarStates.Masomenos;
+        }
+        else if (m_sanidadActual == 2)
+        {
+            barStates = BarStates.MasomenosMal;
+        }
+        else if (m_sanidadActual == 1)
+        {
+            barStates = BarStates.Mal;
+        }
+       
+        ActionManager();
+    }
+    void ActionManager()
+    {
+        switch (barStates)
+        {
+            case BarStates.None:
+                break;
+            case BarStates.Normal:
+                break;
+            case BarStates.MasomenosNormal:
+                break;
+            case BarStates.Masomenos:
+                break;
+            case BarStates.MasomenosMal:
+                break;
+            case BarStates.Mal:
+                break;
 
+        }
+    }
     int BarSanidad(int sanidadActual, int sanidadPerdida)
     {
         sanidadActual -= sanidadPerdida;
-        return sanidadActual;
+        m_sanidadActual = sanidadActual;
+        return m_sanidadActual;
     }
     int BarSanidad(int sanidadActual, int sanidadRecuperada, int sanidad)
     {
         sanidadActual += sanidadRecuperada;
         sanidad = sanidadActual;
-        return sanidad;
+        m_sanidadActual= sanidad;
+        return m_sanidadActual;
     }
     bool imInteractive()
     {
@@ -29,6 +84,16 @@ public class PlayerManager : MonoBehaviour
     }
     void ChangeController()
     {
+
+    }
+    private enum BarStates
+    {
+        None,
+        Normal,
+        MasomenosNormal,
+        Masomenos,
+        MasomenosMal,
+        Mal,
 
     }
 }
