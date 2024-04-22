fe1cfec324f4c059ecfa31943a9334da0cdf4e00
Burmuruk <santoyo.herrera.david@gmail.com>
2024-04-21 21:00:06 -0600

    IMission

--- Assets/Scripts/IMission.cs
+++ Assets/Scripts/IMission.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class IMission : MonoBehaviour
+public interface IMission
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    event Action OnMissionCompleted;
+    void ChangeDay();
 }
